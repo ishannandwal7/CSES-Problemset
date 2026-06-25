341bb777252aac4365eb42e992603894a96d4a81
Ishan Nandwal <ishannandwal7@gmail.com>
2026-06-25 21:52:01 +0530

    Course Schedule  (Toposort) - Kahn's Algo

---
+++ Graph/Course Schedule  (Toposort).cpp
@@ -0,0 +1,147 @@
+#include "bits/stdc++.h"
+#include <algorithm>
+#include <ostream>
+#include <queue>
+#include <string>
+#include <unordered_map>
+using namespace std;
+#define fast_io                                                                \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define ll long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define sz(x) (int)(x).size()
+// const int mod = 1e9 + 7;
+ 
+// int dx[8] = {0,0,-1,-1,-1,1,1,1};
+// int dy[8] = {-1,1,-1,0,1,-1,0,1};
+ 
+//         L, R, U, D
+int dx[4] ={0,0,-1,1};
+int dy[4] ={-1,1,0,0};
+ 
+class DSU {
+  vector<int>size,parent;
+  public:
+  int components;
+  DSU(int n){
+    size.resize(n,1);
+    parent.resize(n);
+    iota(begin(parent),end(parent),0);
+    components = n;
+  }
+  int findParent(int node){
+    if(parent[node]==node){return node;}
+    return parent[node] = findParent(parent[node]);
+  }
+  void merge(int u,int v){
+    int U = findParent(u);
+    int V = findParent(v);
+    if(U==V){return ;}
+    if(size[U]<size[V]){
+      size[V]+=size[U];
+      parent[U]=V;
+    }else{
+      size[U]+=size[V];
+      parent[V]=U;
+    }
+    --components;
+    return ;
+  }
+};
+
+int startNode = -1;
+
+void getCycle(int node,vector<int>&parent){
+  vector<int>cycle;
+  cycle.push_back(startNode);
+  while(parent[node]!=node && node != startNode){
+    cycle.push_back(node);
+    node = parent[node];
+  }
+  cycle.push_back(startNode);
+  reverse(begin(cycle),end(cycle));
+  cout<<cycle.size()<<"\n";
+  for(auto x:cycle){
+    cout<<x<<" ";
+  }
+  return ;
+}
+bool dfs(int node,vector<vector<int>>&adj,vector<bool>&vis,vector<bool>&pathVis,vector<int>&parent){
+  vis[node]=1;
+  pathVis[node]=1;
+  for(auto child:adj[node]){
+    if(!vis[child]){
+      parent[child]=node;
+      if(dfs(child,adj,vis,pathVis,parent)){return 1;}
+    }else{
+      if(pathVis[child]){
+        startNode = child;
+        getCycle(node,parent);
+        return 1;
+      }
+    }
+  }
+  pathVis[node]=0;
+  parent[node] = node;
+  return 0;
+}
+
+void solve() {
+  int n,m;cin>>n>>m;
+  vector<vector<int>>adj(n+1);
+  vector<int>indegree(n+1,0);
+  vector<bool>vis(n+1,0);
+  vector<int>ans;
+  queue<int>q;
+  for(int i=0;i<m;++i){
+    int a,b;cin>>a>>b;
+    adj[a].push_back(b);
+    indegree[b]++;
+  }
+  for(int i=1;i<=n;++i){
+    if(indegree[i]==0){
+      q.push(i);
+    }
+  }
+ 
+  while(!q.empty()){
+    auto head = q.front();
+    q.pop();
+    // vis[head]=1;
+    ans.push_back(head);
+    for(auto x:adj[head]){
+      --indegree[x];
+      if(indegree[x]==0){
+        q.push(x);
+      }
+    }
+  }
+  if((int)ans.size()!=n){cout<<"IMPOSSIBLE\n";return ;}
+  for(auto x:ans){
+      cout<<x<<" ";
+  }
+  return ;
+}
+ 
+ 
+int main() {
+  fast_io;
+#ifndef ONLINE_JUDGE
+  if (fopen("input.txt", "r")) {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+  }
+#endif
+ 
+  int t = 1;
+  // cin >> t;
+ 
+  while (t--) {
+    solve();
+  }
+ 
+  return 0;
+}
